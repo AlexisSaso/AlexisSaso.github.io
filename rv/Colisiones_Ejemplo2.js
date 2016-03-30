e8077bbdcf0e2067dd12ee40019dc3a8d10f9a46
AlexisSaso <alexissaso94@gmail.com>
2016-03-30 11:14:16 -0600

    Update Colisiones_Ejemplo2.js

--- rv/Colisiones_Ejemplo2.js
+++ rv/Colisiones_Ejemplo2.js
@@ -1,7 +1,7 @@
 function setup(){
-  cubo1 = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1),
+  cubo1 = new THREE.Mesh( new THREE.BoxGeometry( 1, 10, 1),
                           new THREE.MeshNormalMaterial());
-  cubo2 = new THREE.Mesh( new THREE.BoxGeometry( 1, 1, 1),
+  cubo2 = new THREE.Mesh( new THREE.BoxGeometry( 1, 10, 1),
                           new THREE.MeshNormalMaterial());
   pelota = new THREE.Mesh( new THREE.SphereGeometry( 0.5 ),
                           new THREE.MeshNormalMaterial());
