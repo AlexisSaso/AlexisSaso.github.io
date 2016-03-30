b9fd50f5409ad3c38fd5f7e4c11b873042558bd1
AlexisSaso <alexissaso94@gmail.com>
2016-03-30 14:10:09 -0600

    Update Colisiones_Ejemplo2.js

--- rv/Colisiones_Ejemplo2.js
+++ rv/Colisiones_Ejemplo2.js
@@ -20,6 +20,8 @@ function setup(){
   
     raycaster1 = new THREE.Raycaster( pelota.position, new THREE.Vector3(1,0,0));
     raycaster2 = new THREE.Raycaster( pelota.position, new THREE.Vector3(-1,0,0));
+    raycaster3 = new THREE.Raycaster( pelota.position, new THREE.Vector3(0,1,0));
+    raycaster4 = new THREE.Raycaster( pelota.position, new THREE.Vector3(0,-1,0));
   
   escena = new THREE.Scene();
   escena.add ( cubo1 );
@@ -34,30 +36,39 @@ function setup(){
                     window.innerHeight*.95 );
   document.body.appendChild( renderer.domElement );
 
-  step = 0.1;
+  stepx = 0.1;
+  stepy = 0.1;
 }
 
 function loop(){
   obstaculo1 = raycaster1.intersectObject( cubo1 );
   obstaculo2 = raycaster2.intersectObject( cubo2 );
+  obstaculo3 = raycaster3.intersectObject( cubo3 );
+  obstaculo4 = raycaster4.intersectObject( cubo4 );
   
   if ((obstaculo1.length > 0 && (obstaculo1[0].distance <= 0.5)) ||
       (obstaculo2.length > 0 && (obstaculo2[0].distance <= 0.5)))
-      step= -step;
+      stepx= -stepx;
       
-  pelota.position.x += step;
-  pelota.position.y += 0.005;
-  pelota.rotation.z += -step;
+  if ((obstaculo3.length > 0 && (obstaculo1[0].distance <= 0.5)) ||
+      (obstaculo4.length > 0 && (obstaculo2[0].distance <= 0.5)))
+      stepy= -stepy;
+      
+  pelota.position.x += stepx;
+  pelota.position.y += stepy;
+  pelota.rotation.z += -stepx;
   raycaster1.set( pelota.position, new THREE.Vector3(1,0,0) );
   raycaster2.set( pelota.position, new THREE.Vector3(-1,0,0) );
+  raycaster3.set( pelota.position, new THREE.Vector3(0,1,0) );
+  raycaster4.set( pelota.position, new THREE.Vector3(0,-1,0) );
   
   renderer.render( escena, camara );
   requestAnimationFrame( loop );
 }
 
-var cubo1, cubo2, pelota, escena, camara, renderer;
-var raycaster1, raycaster2, step;
-var obstaculo1, obstaculo2;
+var cubo1, cubo2, cubo3, cubo4, pelota, escena, camara, renderer;
+var raycaster1, raycaster2, reycaster3, reycaster4, step;
+var obstaculo1, obstaculo2, obstaculo3, obstaculo4;
 
 setup();
 loop();
