d2d33397af018e389f7e41a36d2df5e1f503e813
AlexisSaso <alexissaso94@gmail.com>
2016-04-01 00:08:44 -0600

    Update robot2.js

--- rv/robot2.js
+++ rv/robot2.js
@@ -43,7 +43,7 @@ renderer.render (escena, camara);
 if (Math.abs(pieD.rotation.x) > .3 )
   step = -step;
 
-if (Math.abs(brazoD.rotation.z) > 7 )
+if (Math.abs(brazoD.rotation.z) > 6 )
   stepbrazo = -stepbrazo;
 
 brazoD.rotation.z += stepbrazo;
