9081507a933ff7d2d6f548e8f916fef39d4c8094
AlexisSaso <alexissaso94@gmail.com>
2016-04-01 02:01:20 -0600

    Update robot2.js

--- rv/robot2.js
+++ rv/robot2.js
@@ -1,9 +1,9 @@
 function Completo(){
   THREE.Object3D.call(this);
-  this.cilindroForma = (new THREE.CylinderGeometry(0.5,0.5,4), new THREE.MeshBasicMaterial({color:0xffffff}));
-  this.rectanguloForma = (new THREE.BoxGeometry(0.5,1,2), new THREE.MeshBasicMaterial({color:0xffffff}));
+  this.cilindroForma = (new THREE.CylinderGeometry(0.5,0.5,4),new THREE.MeshBasicMaterial({color:0xffffff}));
+  this.rectanguloForma = (new THREE.BoxGeometry(0.5,1,2),new THREE.MeshBasicMaterial({color:0xffffff}));
   
-  this.cuerpo = new THREE.Mesh(new THREE.SphereGeometry(3), new THREE.MeshBasicMaterial({color:0xffffff}));
+  this.cuerpo = new THREE.Mesh(new THREE.SphereGeometry(3),new THREE.MeshBasicMaterial({color:0xffffff}));
   this.brazoI = new THREE.Mesh(this.cilindroForma);
   this.brazoD = new THREE.Mesh(this.cilindroForma);
   this.pieI = new THREE.Mesh(this.rectanguloForma);
