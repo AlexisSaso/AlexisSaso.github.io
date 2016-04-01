259b5f4466f4d0abf48a38dc5a2172b1d9070997
AlexisSaso <alexissaso94@gmail.com>
2016-04-01 00:06:26 -0600

    Update robot2.js

--- rv/robot2.js
+++ rv/robot2.js
@@ -1,6 +1,6 @@
 function setup(){
   var esferaForma = new THREE.SphereGeometry(3);
-  var cilindroForma = new THREE.CylinderGeometry(0.5,0.5,2);
+  var cilindroForma = new THREE.CylinderGeometry(0.5,0.5,3);
   var rectanguloForma = new THREE.BoxGeometry(0.5,1,2);
   
   var cuerpo = new THREE.Mesh(esferaForma);
