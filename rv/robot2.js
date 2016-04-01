dbdc18bb21d249f982452d0daadd329358836e16
AlexisSaso <alexissaso94@gmail.com>
2016-04-01 00:28:27 -0600

    Update robot2.js

--- rv/robot2.js
+++ rv/robot2.js
@@ -9,8 +9,8 @@ function setup(){
   pieI = new THREE.Mesh(rectanguloForma);
   pieD = new THREE.Mesh(rectanguloForma);
   
-  brazoI.position.x=2.25;
-  brazoD.position.x=-2.25;
+  brazoI.position.x=2.3;
+  brazoD.position.x=-2.3;
   pieD.position.y=-3;
   pieD.position.x=1;
   pieI.position.y=-3;
