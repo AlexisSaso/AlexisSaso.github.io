bba116b45d5445f199c3e175bb7e5a9653d246ad
AlexisSaso <alexissaso94@gmail.com>
2016-04-22 13:38:11 -0500

    Update robotagentes.js

--- rv/robotagentes.js
+++ rv/robotagentes.js
@@ -51,7 +51,7 @@ Environment.prototype.setMap=function(map){
    if(map[i][j]==="x")
     this.add(new Wall(1, j-offset,-(i-offset)));
    else if(map[i][j]==="r")
-    this.add(new BB8(j-offset,-(i-offset)));
+    this.add(new kirby(j-offset,-(i-offset)));
   }
  }
 }	
