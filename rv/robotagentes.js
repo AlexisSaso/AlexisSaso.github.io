25e9089d8561ec2cd299b8e440cbe8289ad45abd
AlexisSaso <alexissaso94@gmail.com>
2016-04-27 10:55:47 -0500

    Update robotagentes.js

--- rv/robotagentes.js
+++ rv/robotagentes.js
@@ -106,7 +106,7 @@ kirby.prototype.operations.Derecho = function(robot,step){
  robot.scale.x=0.5;
  robot.scale.y=0.5;
  robot.scale.z=0.5;
- if (Math.abs(robot.pieD.rotation.z) > Math.PI || Math.abs(robot.pieD.rotation.z) < Math.PI/8)
+ if (Math.abs(robot.pieD.rotation.z) > Math.PI)
   steppie = -steppie;
 
 if (Math.abs(robot.brazoD.rotation.x) > 2 || Math.abs(robot.brazoD.rotation.x) < 1)
