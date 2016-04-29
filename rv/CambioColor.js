ad20f933a00c27017aede556d22931858190f96a
AlexisSaso <alexissaso94@gmail.com>
2016-04-29 13:41:14 -0500

    Update CambioColor.js

--- rv/CambioColor.js
+++ rv/CambioColor.js
@@ -55,7 +55,6 @@ Environment.prototype.setMap=function(map){
     this.add(new Wall(1, j-offset,-(i-offset)));
    else if(map[i][j]==="r")
     this.add(new kirby(j-offset,-(i-offset)));
-    this.add(new luzPuntual(-offset,-(i-offset)));
   }
  }
 }	
